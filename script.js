29c9372dc9168b584775003bcba728986b661f95
Dorian Arriaga <dorianarri@gmail.com>
2023-03-28 17:22:05 -0400

    add api test

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ function generateQuote() {
   })
 }
 
-// const api_url ="https://zenquotes.io/api/quotes/";
+const api_url ="https://zenquotes.io/api/quotes/";
 
-// async function getapi(url)
-// {
-//   const response = await fetch(url);
-//   var data = await response.json();
-//   // console.log(data)
-//   quoteElement.innerHTML = data[Math.floor(Math.random() * 51)].q;
-// }
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  // console.log(data)
+  quoteElement.innerHTML = data[Math.floor(Math.random() * 51)].q;
+}
 
-// getapi(api_url);
+getapi(api_url);
