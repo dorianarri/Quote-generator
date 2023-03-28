3b053a8e32f5fcb6c75ada63589f0923c1cdd499
Dorian Arriaga <dorianarri@gmail.com>
2023-03-28 17:19:21 -0400

    first

---
+++ script.js
@@ -0,0 +1,26 @@
+const quoteElement = document.getElementById('quote');
+const quoteBtn = document.getElementById('quotebtn');
+
+quoteBtn.addEventListener('click', generateQuote)
+
+generateQuote()
+
+function generateQuote() {
+  fetch("https://zenquotes.io/api/quotes/")
+  .then((res) => res.json())
+  .then((data) => {
+    quoteElement.innerHTML = data[Math.floor(Math.random() * 51)].q;
+  })
+}
+
+// const api_url ="https://zenquotes.io/api/quotes/";
+
+// async function getapi(url)
+// {
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   // console.log(data)
+//   quoteElement.innerHTML = data[Math.floor(Math.random() * 51)].q;
+// }
+
+// getapi(api_url);
